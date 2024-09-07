286305cb0e627d090b7cbb825edc23c78d46ec6a
Anthony <ndegwaanthony300@gmail.com>
2024-09-07 16:17:28 +0300

    implemenet a successful route for the week scheduled tasks

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
-# Initializes the Flask app and extensions
+from flask import Flask
+
+# Function to create and configure the app
+def create_app():
+    app = Flask(__name__)
+    
+    # Import the routes after the app is created
+    from .routes import main_bp
+    
+    # Register blueprint
+    app.register_blueprint(main_bp)
+
+    return app
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,13 @@
-# Define routes for the application
+from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
+
+
+main_bp = Blueprint('homepage', __name__)
+# main_bp.config['SECRETE KEY'] = 'mysecrete key'
+
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
